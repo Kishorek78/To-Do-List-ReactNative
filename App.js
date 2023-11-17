3cdd80ef3cb91074fb41d0f2b8133e1e83b03336
kishore_78s <wwwkishoresanjeev213@gmail.com>
2023-11-17 06:31:12 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,81 @@
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button,TextInput,inputContainer,appContainer,goalsContainer ,ScrollView,FlatList,ImageBackground} from 'react-native';
+import GoalItem from './GoalItem';
+import GoalInput from './GoalInput';
+import { StatusBar } from 'expo-status-bar';
+
+export default function(app) {
+
+const [goals,setGoals]=useState([]); 
+const [isModalVisible, setIsModalVisible]=useState(false);
+
+
+function addGoalHandler(goalText) {
+  setGoals((currentGoals)=>[...currentGoals,goalText])
+
+}
+function deleteItem(index)
+{
+  
+  const newGoals=goals.filter((el,i)=> i != index )
+  setGoals(newGoals);
+}
+function startAddGoalHandler(){
+
+setIsModalVisible(true);
+
+}
+function closeGoalHandler(){
+
+  setIsModalVisible(false);
+  
+  }
+
+  return (
+    <>
+     <StatusBar style={'light'} />
+ <View style={styles.appContainer}>
+ 
+  <Button title='Add New Goal' color="#A070D6" onPress={startAddGoalHandler} />
+   <GoalInput  onClose={closeGoalHandler} visible={isModalVisible} onAddGoal={addGoalHandler}/> 
+      <View style={styles.goalsContainer}>
+    
+      <FlatList 
+      
+      data={goals} 
+      renderItem={(itemData) =>{
+        return <GoalItem text={itemData.item} onDelete={() => deleteItem(itemData.index)}/>
+}}
+
+      
+      
+      />
+      </View>
+     
+  </View>
+    </>
+   
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    paddingTop:50,
+    paddingHorizontal:16,
+    flex:1,
+    backgroundColor:'#1A0037',
+    
+    
+  },
+  
+  goalsContainer:{
+  flex:4
+
+  },
+
+ 
+ 
+  
+
+
+});
